9ed0755d79b0dcd267d8c0c755382f437052b4e1
Felix Dion Robidoux <diglididude.ng@gmail.com>
2016-02-16 15:55:24 -0500

    Added Memory object.

---
+++ Memory.js
@@ -0,0 +1,30 @@
+/**
+ * TODO: Memory documentation.
+ * 
+ * @class
+ * @constructor
+ */
+Memory = function() { };
+
+Memory = {
+    /**
+     * TODO: Memory documentation.
+     * @type {object<string, Creep>}
+     */
+    creeps: {},
+    /**
+     * TODO: Memory documentation.
+     * @type {object<string, Room>}
+     */
+    rooms: {},
+    /**
+     * TODO: Memory documentation.
+     * @type {object<string, Spawn>}
+     */
+    spawns: {},
+    /**
+     * TODO: Memory documentation.
+     * @type {object<string, Flag>}
+     */
+    flags: {}
+};
